24619785b075c85daaa1c5078e4439dfb61b052c
cruz rodriguez <cruzrodriguez1127@gmail.com>
2019-03-19 23:27:48 -0700

    completed excercises-1

--- excercises-1.js
+++ excercises-1.js
@@ -32,6 +32,9 @@
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
+function sum(a, b) {
+  return (a + b);
+}
 console.assert(sum(2,4) === 6)
 console.assert(sum(45, 23) === 68)
 console.assert(sum(-2, -57) === -59)
@@ -40,6 +43,9 @@ console.assert(sum(-2, -57) === -59)
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
+function avg(a, b, c) {
+  return ((a + b + c)/3);
+}
 console.assert(avg(2,3,4) === 3)
 console.assert(avg(234, 63, 234) === 177)
 console.assert(avg(123,234,345) === 234)
@@ -48,6 +54,9 @@ console.assert(avg(123,234,345) === 234)
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
+function getLength(str) {
+  return str.length;
+}
 console.assert(getLength('Michael') === 7)
 console.assert(getLength('Javascript') === 10)
 console.assert(getLength('supercalifragilisticexpialidocious') === 34)
@@ -58,6 +67,9 @@ console.assert(getLength('supercalifragilisticexpialidocious') === 34)
 // parameter is greater than the second. Otherwise
 // the function should return `false`.
 
+function greaterThan(a, b) {
+  return (a > b);
+}
 console.assert(greaterThan(2, 1) === true)
 console.assert(greaterThan(23, 23) === false)
 console.assert(greaterThan(-234, 1) === false)
@@ -68,6 +80,9 @@ console.assert(greaterThan(-234, 1) === false)
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 
+function greet(str) {
+  return ("Hello, " + str + "!");
+}
 console.assert(greet("Michael") === "Hello, Michael!")
 console.assert(greet("R2D2") === "Hello, R2D2!")
 console.assert(greet("Iron Man") === "Hello, Iron Man!")
@@ -83,6 +98,14 @@ console.assert(greet("Iron Man") === "Hello, Iron Man!")
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 
+function madlib(str1, str2, str3, str4) {
+  if (typeof str1 === "string" && typeof str2 === "string" && typeof str3 === "string" && typeof str4 === "string") {
+    return ("The " + str1 + " brown " + str2 + " " + str3 + " over the " + str4 + ".");
+  }
+  else {
+    return "not a string."
+  }
+}
 console.assert(madlib("quick","fox","jumps","fence")=== "The quick brown fox jumps over the fence.")
 console.assert(madlib("fat","rabbit","hops","wall")=== "The fat brown rabbit hops over the wall.")
 ///////////////////////////////////////////////////////
