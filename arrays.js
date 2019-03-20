e0e3a0e96fca615b23afb5704ce4b333805b74f5
cruz rodriguez <cruzrodriguez1127@gmail.com>
2019-03-19 23:29:15 -0700

    attempted arrays

--- arrays.js
+++ arrays.js
@@ -19,27 +19,63 @@ var instructors = [
 // 1. Find largest number
 // ---------------------------
 
+var largestNum = Math.max(...numbers); //Math.max(); - checks for the the largest number
+console.log(largestNum);
+
+
 
 // ---------------------------
 // 2. Find longest string
 // ---------------------------
 
+function longestStr (array) {
+  var longestWord = "";
+  array.forEach(function(strings) { //.forEach(); - takes each element of an array once 
+    if(strings.length > longestWord.length) {
+      longestWord = strings;
+    }
+  });
+  return longestWord;
+}
+var strings = longestStr(['this','is','a','collection','of','words']);
+console.log(strings);
 
 // ---------------------------
 // 3. Find even numbers
 // ---------------------------
+function evenNum (numbers) {
+  var arr = [];
+  for(var i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 === 0) {
+      arr.push(numbers[i]); //.push(); - pushes a number to the top of an array
+    }
+  }
+  return arr;
+}
 
+var even = evenNum([1,12,4,18,9,7,11,3,101,5,6]);
+console.log(even);
 
 
 // ---------------------------
 // 4. Find odd numbers
 // ---------------------------
+function oddNum (numbers) {
+  var arr = [];
+  for(var i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+      arr.push(numbers[i]); //.push(); - pushes a number to the top of an array
+    }
+  }
+  return arr;
+}
 
+var odd = oddNum([1,12,4,18,9,7,11,3,101,5,6]);
+console.log(odd);
 
 // ---------------------------
 // 5. Find words that contain `is`
 // ---------------------------
-
 
 
 // ---------------------------
